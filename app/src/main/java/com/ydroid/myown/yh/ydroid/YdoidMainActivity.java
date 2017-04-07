495b8e4701f3b4b0cf919c07c0f99f324d8c6cd3
yh <yanghuancoder@163.com>
2017-04-07 08:48:39 +0800

    initial the ydroid project

---
+++ app/src/main/java/com/ydroid/myown/yh/ydroid/YdoidMainActivity.java
@@ -0,0 +1,13 @@
+package com.ydroid.myown.yh.ydroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YdoidMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_container);
+    }
+}
